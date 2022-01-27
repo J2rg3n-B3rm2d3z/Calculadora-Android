cfe3753a09d4b9cf43595d8beb4ccead1998f9ff
Jurgen hace programas <jurgenfbermudez@gmail.com>
2022-01-26 19:20:16 -0600

    Se agrego una nueva Activity y mas codigo

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +15,51 @@ public class MainActivity extends AppCompatActivity {
     private EditText Num1;
     private EditText Num2;
 
+    //Metodos de control de activity
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"OnStart",Toast.LENGTH_SHORT).show();
+        //La actividad esta creada
 
         Resultado = findViewById(R.id.Respuesta);
         Num1 = findViewById(R.id.Numero_1);
         Num2 = findViewById(R.id.Numero_2);
     }
 
-    //Metodos a utilizar
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this,"OnStart",Toast.LENGTH_SHORT).show();
+        //La actividad esta a punto de hacerse visible
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this,"OnResume",Toast.LENGTH_SHORT).show();
+        //La actividad se ha vuelto visible (ahora se "reanuda")
+    }
+
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this,"OnPause",Toast.LENGTH_SHORT).show();
+        //Enfocarse en otra actividad (esta actividad esta a punto de ser detenida)
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(this,"OnStop",Toast.LENGTH_SHORT).show();
+        //La actividad ya no es visible
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this,"OnDestroy",Toast.LENGTH_SHORT).show();
+        //La actividad esta a punto de ser destruida
+    }
+
+    //Metodos de validacion
 
     private boolean Validar()
     {
@@ -40,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
         Resultado.setTextSize(40);
         return true;
     }
+
+    //Operaciones matematicas para la calculadora
 
     public void Suma (View view)
     {
@@ -73,5 +111,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Metodos para cambios de Activity
 
+    public void Acercade (View view)
+    {
+        Intent i = new Intent(this, Politicas.class);
+        startActivity(i);
+    }
 }
